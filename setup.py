14324dbe9dd27b8913a961611ff202e1a46f4d8e
Neil Vaytet <neil.vaytet@esss.se>
2020-05-27 15:07:48 +0200

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dress",
-    version="0.0.1",
+    version="0.0.2",
     author="Neil Vaytet",
     author_email="neil.vaytet@esss.se",
     description="Data Reduction for ESS",
